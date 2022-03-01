0e72ef07cf54bb68eb1843e2170fab1e073b986a
Fortune <fortunemuhumuza@gmail.com>
2022-03-01 13:28:16 +0300

    added logging number of online users with socketio

--- api/app.js
+++ api/app.js
@@ -1,55 +1,86 @@
-const express = require('express');
+const app = require('express')
+const http = require('http').createServer(app)
+const io = require('socket.io')(http, {
+  cors: {
+    origin: "http://localhost:3000"
+  }
+})
 
-// create express app
-const app = express();
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-        'Access-Control-Allow-Methods',
-        'OPTIONS, GET, POST, PUT, PATCH, DELETE'
-    );
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+// io.on('connection', socket => {
+//   socket.on('message', ({ name, message }) => {
+//     io.emit('message', { name, message })
+//   })
+// })
+
+let onlineUsers = 0;
+
+io.on('connection', (socket) => {
+  //console.log('a user connected');
+  onlineUsers = onlineUsers+1
+  console.log(onlineUsers, 'online')
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
-app.use(express.json());
-app.use(express.urlencoded({
-    extended: true
-}));
 
-require('./routes/userRoutes.js')(app);
-require('./routes/groupRoutes.js')(app)
+http.listen(4000, () => {
+  console.log('listening on port 4000')
+})
+
+// const express = require('express');
+
+// // create express app
+// const app = express();
+
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader(
+//         'Access-Control-Allow-Methods',
+//         'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+//     );
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+// });
+
+// app.use(express.json());
+// app.use(express.urlencoded({
+//     extended: true
+// }));
+
+// require('./routes/userRoutes.js')(app);
+// require('./routes/groupRoutes.js')(app)
 
 
 
-// Configuring the database
-const dbConfig = require('./config/dbConfig.js');
-const mongoose = require('mongoose');
+// // Configuring the database
+// const dbConfig = require('./config/dbConfig.js');
+// const mongoose = require('mongoose');
 
 
 
 
 
 
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
 
-// Connecting to the database
-mongoose.connect(dbConfig.url, {
-    useNewUrlParser: true
-}).then(() => {
-    console.log("Successfully connected to the database");
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
-    process.exit();
-});
+// // Connecting to the database
+// mongoose.connect(dbConfig.url, {
+//     useNewUrlParser: true
+// }).then(() => {
+//     console.log("Successfully connected to the database");
+// }).catch(err => {
+//     console.log('Could not connect to the database. Exiting now...', err);
+//     process.exit();
+// });
 
-// define a simple route
-app.get('/', (req, res) => {
-    res.json({ "message": "online" });
-});
-const port = process.env.PORT || 4000
-// listen for requests
-app.listen(port, () => {
-    console.log("Server is listening on port 4000");
-});
+// // define a simple route
+// app.get('/', (req, res) => {
+//     res.json({ "message": "online" });
+// });
+// const port = process.env.PORT || 4000
+// // listen for requests
+// app.listen(port, () => {
+//     console.log("Server is listening on port 4000");
+// });
--- frontend/src/components/FullList.js
+++ frontend/src/components/FullList.js
@@ -7,8 +7,9 @@ import './FullList.css'
 import Button from '@mui/material/Button';
 import { useSelector, useDispatch } from 'react-redux'
 import { setUsers } from '../store/reducers/usersSlice';
+import { io } from "socket.io-client";
 
-
+const socket = io.connect('http://localhost:4000')
 
 export default function FullList() {
 
@@ -16,8 +17,15 @@ export default function FullList() {
     const loadingStatus = useSelector(state => state.users.status)
     const records = useSelector(state => state.users.records)
 
+    
+    const connectSocket = () => {
+        socket.emit('message', {message: 'user connected'})
+    }
 
 
+    useEffect(() => {
+      connectSocket()
+    }, [])
 
 
     useEffect(() => {
